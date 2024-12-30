cf6d2e44c1e08068b001cf78bc05898ebc124d21
kajalBurghate <154863153+kajalBurghate@users.noreply.github.com>
2024-12-30 15:20:24 +0530

    Add files via upload

---
+++ shutdownPC.py
@@ -0,0 +1,8 @@
+# Shutdown Computer using python
+
+import os
+
+def Shutdown_PC() :
+    os.system("System /s /t 1")
+
+shutdown_PC()
---
+++ stringReverse.py
@@ -0,0 +1,10 @@
+# Reverse string using python
+
+def reverse_string(string) :
+    return string[::-1]
+
+letter = input("Enter String :")
+
+letter = reverse_string(letter)
+
+print(letter)
